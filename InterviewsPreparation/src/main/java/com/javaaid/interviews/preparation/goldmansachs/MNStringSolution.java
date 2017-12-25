46c3842ea6a0f8842bb58154c07ce4f3c3f3c8ce
Kanahaiya Gupta <kanhaiyagupta20@gmail.com>
2017-12-26 00:44:21 +0530

    Added problem statement on top of the class

--- InterviewsPreparation/src/main/java/com/javaaid/interviews/preparation/goldmansachs/MNStringSolution.java
+++ InterviewsPreparation/src/main/java/com/javaaid/interviews/preparation/goldmansachs/MNStringSolution.java
@@ -1,6 +1,27 @@
 /**
- * 
+ * problem statement-
+ * ------------------------------------
+ * Question:
+	Given a Pattern containing only Ns and M’s . N represents ascending and M represents descending , Each character (M or
+	N) needs to display sequence of numbers(2 numbers) explaining the ascending or descending order (for ex: 21 -
+	>represents descending -> M) . The second character in the pattern takes the last digit from first character and builds the
+	sequence and so on..Please look at example section below.
+	There could be multiple numbers satisfying the pattern. The goal is to find out the lowest numeric value following the
+	pattern.
+	Constraints:
+	Input can have maximum 8 characters
+	Output can have Digits from 1-9 and Digits can’t repeat.
+	In case of no possible output or incorrect input value (like blank /null /NON M or N character) please return -1.
+	Example Section:
+	Input : M
+	Output: 21 (2 -> 1 shows descending and possible smallest numeric value. Even 65 or 74 can qualify, but 21 being the
+	smallest numeric value is the correct answer)
+	Input : MNM
+	Output:2143 (M represents descending 2->1 , N represents ascending 1->4 (1 is coming from last character) , M
+	represents descending 4->3(4 is coming from last character sequence) -- There would be many number qualifying the
+	pattern like 3142 ,8796,6241 etc.. 2143 is the lowest numeric value for this pattern sequence.)
  */
+
 package com.javaaid.interviews.preparation.goldmansachs;
 
 import java.io.BufferedWriter;
--- InterviewsPreparation/src/main/java/com/javaaid/interviews/preparation/goldmansachs/NumberChecker.java
+++ InterviewsPreparation/src/main/java/com/javaaid/interviews/preparation/goldmansachs/NumberChecker.java
@@ -1,4 +1,20 @@
 /**
+*problem statement-
+	-------------------------------------
+	Given an integer array as input, find out the integers which contains the digits 1,2,3 numbers in its formation . The order of 1,2,3 need
+	not be sequential.
+	In case multiple numbers qualifies the condition, The outcome should be ordered based on numeric value (lesser number first followed
+	by greater number and so on..) and separated by comma.
+	In case no number qualifies the conditions, it should return -1.
+	Example:
+	Input :
+	1456
+	345671
+	43218
+	123
+	Output: 123,43218
+	Explanation: In the above example, as there are 2 numbers(43218,123) which qualifies the condition, we need to sort the output in
+	ascending order seperated by comma.
  * 
  */
 package com.javaaid.interviews.preparation.goldmansachs;
